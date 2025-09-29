750f8dc2fb663ae488f39c0ee4b2a34612beb3a1
ValenGomezz <valentina.gomez.fara@gmail.com>
2025-09-29 14:08:00 -0300

    Tarjetas de premios en la pagina PREMIO. Tengo que configurar bien el tema de imagenes

--- TP_Promo_Equipo5B/Premio.aspx
+++ TP_Promo_Equipo5B/Premio.aspx
@@ -3,6 +3,30 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
 <h1 class="text-center fw-bold">Elejí tu premio</h1>
 
+<div class="row row-cols-1 row-cols-md-3 g-4">
+    <asp:Repeater ID="RepRepetidor" runat="server" OnItemDataBound="RepRepetidor_ItemDataBound">
+        <ItemTemplate>
+             <div class="col">
+                <div class="card">
+                     <%-- REPEATER ANIDADO Para mostrar las imagenes --%>
+                        <asp:Repeater ID="RepImagenes" runat="server">
+                            <ItemTemplate>
+                                <img src="<%# Eval("Url") %>" class="card-img-top" alt="Imagen de premio">
+                            </ItemTemplate>
+                        </asp:Repeater>
+                        <%-- FIN REPEATER ANIDADO --%>
+                 <div class="card-body">
+                    <h5 class="card-title">"<%#Eval("Nombre")%>"</h5>
+                    <p class="card-text">"<%#Eval("Descripcion")%>"</p>                  
+                 </div>
+                 </div>
+                 </div>
+        </ItemTemplate>
+    </asp:Repeater>
+    
+ 
+  </div>
+
 
 </asp:Content>
 
--- TP_Promo_Equipo5B/Premio.aspx.cs
+++ TP_Promo_Equipo5B/Premio.aspx.cs
@@ -4,14 +4,42 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using negocio;
+using dominio;
 
 namespace TP_Promo_Equipo5B
 {
     public partial class Premio : System.Web.UI.Page
     {
+        public  List<Articulo> ListaDePremios { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack) 
+            {
+                ArticuloNegocio negocio = new ArticuloNegocio();
 
+                ListaDePremios = negocio.listar();
+
+                RepRepetidor.DataSource = ListaDePremios;
+                RepRepetidor.DataBind();
+            }
+        }
+
+        // Metodo para poder cargar las imagenes anidadas
+        protected void RepRepetidor_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+                Articulo articulo = (Articulo)e.Item.DataItem;
+
+                Repeater repImagenes = (Repeater)e.Item.FindControl("RepImagenes");
+
+                if (repImagenes != null)
+                {
+                    repImagenes.DataSource = articulo.Imagenes;
+                    repImagenes.DataBind();
+                }
+            }
         }
     }
 }
